f036c41fdb25e1aa4ab9b5d1120580de5f042ef5
HsiaoCz <984274788@qq.com>
2023-08-15 17:49:24 +0800

    new project

--- iml/model/user.go
+++ iml/model/user.go
@@ -1,1 +1,11 @@
 package model
+
+import "gorm.io/gorm"
+
+// user Model
+type User struct {
+	gorm.Model
+	Identity int64  `gorm:"column:identity;type:int(11);" json:"identity"`
+	Username string `gorm:"column:username;type:varchar(20);" json:"username"`
+	Password string `gorm:"column:password;type:varchar(50);" json:"password"`
+}
