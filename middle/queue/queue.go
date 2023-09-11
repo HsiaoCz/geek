b8b28aa323f917d020cbfd8d26ea428db461ab3f
HsiaoCz <984274788@qq.com>
2023-09-11 22:08:36 +0800

    middle add queue but very sample now

--- middle/queue/queue.go
+++ middle/queue/queue.go
@@ -1,7 +1,36 @@
 package queue
 
+import (
+	"sync"
+
+	"github.com/HsiaoCz/geek/middle/model"
+)
+
 // 这里实现一个队列
 // 用户发帖后，会放入队列里面
 // 管理员可以从队列里面取文章进行人工审核
 // 人工审核完成后，可以提交发布，也就是可以进入数据库
 // 否则会退回给用户，只有用户删除之后，才会真正的删除文章
+
+// ArtQueue  这个队列只能存article这种数据
+// 添加一个lock字段，避免并发执行的竞态问题
+type ArtQueue struct {
+	data []model.Article
+	lock sync.Mutex
+}
+
+// InQueue 入队
+func (a *ArtQueue) InQueue(article model.Article) {
+	a.lock.Lock()
+	defer a.lock.Unlock()
+	a.data = append(a.data, article)
+}
+
+// OutQueue 出队
+func (a *ArtQueue) OutQueue() model.Article {
+	a.lock.Lock()
+	defer a.lock.Unlock()
+	article := a.data[0]
+	a.data = a.data[1:]
+	return article
+}
