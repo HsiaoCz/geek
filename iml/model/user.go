529dc03cd0aca571444ba0c4fb48af023dd2dcdb
HsiaoCz <984274788@qq.com>
2023-09-13 22:17:59 +0800

    geek

--- iml/model/user.go
+++ iml/model/user.go
@@ -9,5 +9,4 @@ type User struct {
 	Username string `gorm:"column:username;type:varchar(20);" json:"username"`
 	Password string `gorm:"column:password;type:varchar(50);" json:"password"`
 	Content  string `gorm:"column:content;type:varchar(50)" json:"content"`
-	
 }
