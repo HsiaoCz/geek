5569867b20c27ad0097fcdf5a768af6e80c440e6
HsiaoCz <984274788@qq.com>
2023-09-22 19:58:20 +0800

    img

--- img/settings/genid.go
+++ img/settings/genid.go
@@ -1,7 +1,13 @@
 package settings
 
+import (
+	"math/rand"
+	"time"
+)
+
 // GenIdentity 生成用户id
 
 func GenIdentity() int {
-
+	randm := rand.New(rand.NewSource(time.Now().UnixNano()))
+	return randm.Intn(100000000)
 }
--- img/settings/md5.go
+++ img/settings/md5.go
@@ -1,9 +1,16 @@
 package settings
 
+import (
+	"crypto/md5"
+	"encoding/hex"
+)
+
 var sercet = []byte("xiaofanyi")
 
 // GenMD5 对用户的密码进行加密
 
-func GenMD5() string {
-
+func GenMD5(password string) string {
+  h:=md5.New()
+  h.Write(sercet)
+  return hex.EncodeToString(h.Sum([]byte(password)))
 }
