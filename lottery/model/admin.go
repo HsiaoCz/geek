19e6791c7ad537745402ea4a216405977fe12ed4
HsiaoCz <984274788@qq.com>
2023-10-05 21:33:06 +0800

    lottery

--- lottery/model/admin.go
+++ lottery/model/admin.go
@@ -1,5 +1,11 @@
 package model
 
-type Admin struct{
-	
+type Admin struct {
+	Identity string `gorm:"column:identity;type:varchar(20);" json:"identity"`
+	Username string `gorm:"column:username;type:varchar(20);" json:"username"`
+	Passwrod string `gorm:"column:password;type:varchar(20);" json:"passwrod"`
+}
+
+func (a Admin) TableName() string {
+	return "admin"
 }
--- lottery/model/user.go
+++ lottery/model/user.go
@@ -1,5 +1,12 @@
 package model
 
-type User struct{
-	
+type User struct {
+	Identity string `gorm:"column:identity;type:varchar(20);" json:"identity"`
+	Username string `gorm:"column:username;type:varchar(20);" json:"username"`
+	Passwrod string `gorm:"column:password;type:varchar(20);" json:"passwrod"`
+	// 这里需要订阅一个消息队列
+}
+
+func (u User) TableName() string {
+	return "user"
 }
