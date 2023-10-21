5433b826f66f26fce3516687412be357db13c6e5
HsiaoCz <984274788@qq.com>
2023-10-21 22:13:10 +0800

    anymore

--- anymore/router.go
+++ anymore/router.go
@@ -1,21 +1,33 @@
 package anymore
 
 import (
-	"log"
 	"net/http"
+	"strings"
 )
 
 type router struct {
+	// 这里使用的是map
+	// 其实也可以使用切片
+	// 这里一个方法对应一个路由树
+	roots    map[string]*node
 	handlers map[string]HandleFunc
 }
 
 func newRouter() *router {
-	return &router{handlers: make(map[string]HandleFunc)}
+	return &router{
+		roots:    make(map[string]*node),
+		handlers: make(map[string]HandleFunc),
+	}
 }
 
 func (r *router) addRouter(method string, pattern string, handler HandleFunc) {
-	log.Printf("Route %s - %s", method, pattern)
+	parts := parsePattern(pattern)
 	key := method + "-" + pattern
+	_, ok := r.roots[method]
+	if !ok {
+		r.roots[method] = &node{}
+	}
+	r.roots[method].insert(pattern, parts, 0)
 	r.handlers[key] = handler
 }
 
@@ -26,4 +38,47 @@ func (r *router) hanle(c *Context) {
 	} else {
 		c.String(http.StatusNotFound, "404 NOT FOUND:%s\n", c.Path)
 	}
+}
+
+func (r *router) getRoute(method string, path string) (*node, map[string]string) {
+	searchParts := parsePattern(path)
+	params := make(map[string]string)
+	root, ok := r.roots[method]
+
+	if !ok {
+		return nil, nil
+	}
+
+	n := root.search(searchParts, 0)
+
+	if n != nil {
+		parts := parsePattern(n.pattern)
+		for index, part := range parts {
+			if part[0] == ':' {
+				params[part[1:]] = searchParts[index]
+			}
+			if part[0] == '*' && len(part) > 1 {
+				params[part[1:]] = strings.Join(searchParts[index:], "/")
+				break
+			}
+		}
+		return n, params
+	}
+
+	return nil, nil
+}
+
+// 通配符匹配
+func parsePattern(pattern string) []string {
+	vs := strings.Split(pattern, "/")
+	parts := make([]string, 0)
+	for _, item := range vs {
+		if item != "" {
+			parts = append(parts, item)
+			if item[0] == '*' {
+				break
+			}
+		}
+	}
+	return parts
 }
