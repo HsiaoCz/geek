ef4b8225032b06e52bd80d6ac588226114de0511
HsiaoCz <984274788@qq.com>
2023-11-04 16:03:30 +0800

    Tenz

--- Tenz/main.go
+++ Tenz/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"log"
 
+	"github.com/HsiaoCz/geek/Tenz/router"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	r := fiber.New()
+	router.RegisRoute(r)
 	log.Fatal(r.Listen("127.0.0.1:9911"))
 }
