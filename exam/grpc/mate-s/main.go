cce3aed6138b3038910ac2992752a73e2a612cb8
HsiaoCz <984274788@qq.com>
2023-12-08 20:15:47 +0800

    grpc

---
+++ exam/grpc/mate-s/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"log"
+	"math/rand"
+	"net"
+	"time"
+
+	"github.com/HsiaoCz/geek/exam/grpc/mate-s/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+)
+
+type Service struct {
+	pb.UnimplementedGreeterServer
+}
+
+func (s *Service) UserLogin(ctx context.Context, in *pb.LoginRequest) (*pb.LoginResponse, error) {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return nil, status.Errorf(codes.DataLoss, "UserLogin:need the metadata")
+	}
+	if t, ok := md["token"]; ok {
+		if len(t) < 1 || t[0] != "app-test-hisaocz" {
+			return nil, status.Errorf(codes.Unauthenticated, "认证失败")
+		}
+	}
+	if in.GetPasswrod() != in.GetRepassword() {
+		return nil, status.Errorf(codes.OK, "请检查用户名和密码")
+	}
+	return &pb.LoginResponse{Identity: GenIdentity(), Msg: "登录成功"}, nil
+}
+
+func GenIdentity() int64 {
+	randm := rand.New(rand.NewSource(time.Now().UnixNano()))
+	return randm.Int63n(1000000)
+}
+
+var (
+	network = "tcp"
+	addr    = "127.0.0.1:9001"
+)
+
+func main() {
+	listen, err := net.Listen(network, addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &Service{})
+	if err := s.Serve(listen); err != nil {
+		log.Fatal(err)
+	}
+}
